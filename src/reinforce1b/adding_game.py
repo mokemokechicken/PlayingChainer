fd5d974596f4f9339b8d1656b7746d426f633341
Ken Morishita <k_morishita@yumemi.co.jp>
2015-07-15 23:35:52 +0900

    できたかな？

--- src/reinforce1b/adding_game.py
+++ src/reinforce1b/adding_game.py
@@ -92,9 +92,9 @@ class HumanPlayer(object):
         return int(action)
 
 class NNQLearningPlayer(object):
-    ALPHA = 0.05
+    ALPHA = 0.1
     GAMMA = 0.99
-    E_GREEDY = 0.1
+    E_GREEDY = 0.3
 
     def __init__(self):
         self.actions = [1, 2, 3, 4]
@@ -105,25 +105,22 @@ class NNQLearningPlayer(object):
         )
         self.optimizer = optimizers.SGD()
         self.optimizer.setup(self.model.collect_parameters())
-        self.last_state = self.last_action = None
+        self.last_action = None
         self.last_q_list = None
         self.training = True
 
-    def action(self, state_, last_reward):
-        state = state_
-        if self.last_state is not None and self.training:
-            self.update_q_table(self.last_state, self.last_action, state, last_reward)
+    def action(self, state, last_reward):
+        if self.last_action is not None and self.training:
+            self.update_q_table(self.last_action, state, last_reward)
         next_action = self.select_action(state)
-        self.last_state = state
         self.last_action = next_action
         return self.actions[next_action]
 
-    def forward(self, state, volatile=False):
-        x = Variable(np.array([state], dtype=np.int32), volatile=volatile)
+    def forward(self, state):
+        x = Variable(np.array([state], dtype=np.int32))
+        y = None
         for i in range(1, 1000):  # 1000 は適当な数
             if hasattr(self.model, "l%d" % i):
-                # if i > 1:
-                #     x = F.relu(x)
                 x = getattr(self.model, "l%d" % i)(x)
             else:
                 y = x
@@ -135,17 +132,16 @@ class NNQLearningPlayer(object):
         if self.training and random() < self.E_GREEDY:  # http://www.sist.ac.jp/~kanakubo/research/reinforcement_learning.html
             return randint(0, len(self.actions)-1)
         else:
-            # actions = self.forward(state, volatile=True)
             return np.argmax(self.last_q_list.data)
 
-    def update_q_table(self, last_state, last_action, cur_state, last_reward):
-        target_val = last_reward + self.GAMMA * np.max(self.forward(cur_state, volatile=True).data)
+    def update_q_table(self, last_action, cur_state, last_reward):
+        target_val = last_reward + self.GAMMA * np.max(self.forward(cur_state).data)
         self.optimizer.zero_grads()
-        q_last = self.last_q_list
-        tt = np.copy(q_last.data)
+        # 結構無理やりLossを計算・・・ この辺の実装は自信がない
+        tt = np.copy(self.last_q_list.data)
         tt[0][last_action] = target_val
         target = Variable(tt)
-        loss = 0.5 * (target - q_last) ** 2
+        loss = 0.5 * (target - self.last_q_list) ** 2
         loss.grad = np.array([[self.ALPHA]], dtype=np.float32)
         loss.backward()
         self.optimizer.update()
